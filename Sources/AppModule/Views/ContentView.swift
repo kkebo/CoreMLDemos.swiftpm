fce796fb0109d9c7725ce4c374a60a5456f00c5e
Kenta Kubo <kkk669@users.noreply.github.com>
2022-10-29 23:57:14 +0900

    Replace NavigationView with NavigationSplitView

--- Sources/AppModule/Views/ContentView.swift
+++ Sources/AppModule/Views/ContentView.swift
@@ -1,19 +1,42 @@
 import SwiftUI
 
-struct ContentView: View {
+enum Demo {
+    case imageClassification
+    case objectDetection
+}
+
+extension Demo: CaseIterable {}
+
+extension Demo: Identifiable {
+    var id: Self { self }
+}
+
+extension Demo: CustomStringConvertible {
+    var description: String {
+        switch self {
+        case .imageClassification: return "Image Classification"
+        case .objectDetection: return "Object Detection"
+        }
+    }
+}
+
+struct ContentView {
+    @State private var item: Demo?
+}
+
+extension ContentView: View {
     var body: some View {
-        NavigationView {
-            List {
-                NavigationLink("Image Classification") {
-                    EmptyView()
-                }
-                .disabled(true)
-                NavigationLink("Object Detection") {
-                    ObjectDetectionView()
-                }
+        NavigationSplitView {
+            List(Demo.allCases, selection: self.$item) {
+                NavigationLink(LocalizedStringKey($0.description), value: $0)
             }
             .navigationTitle("Core ML Demos")
-            Text("Choose an item from the sidebar.")
+        } detail: {
+            switch self.item {
+            case .imageClassification: EmptyView()
+            case .objectDetection: ObjectDetectionView()
+            case .none: Text("Choose an item from the sidebar.")
+            }
         }
     }
 }
